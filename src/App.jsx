1d9b979c0bbe7312d7b522ad10b09d43a9df784d
Amal Thomas <amalthomas333444@gmail.com>
2025-07-30 10:31:11 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+     <h1 class="text-3xl font-bold underline ">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
